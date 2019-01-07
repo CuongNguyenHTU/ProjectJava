3dc09dfc43258f3d4231b003c14e5a249b5514c7
CuongNguyenHTU <cuongnguyenhtu@gmail.com>
2019-01-08 00:19:06 +0700

    GUI

---
+++ src/ims/dal/Employee_ManagementDAL.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ims.dal;
+
+
+
+/**
+ *
+ * @author Demon
+ */
+public class Employee_ManagementDAL {
+    public Employee_ManagementDAL(){
+    
+    }
+    
+}
